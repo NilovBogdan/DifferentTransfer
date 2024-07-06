c76361ad8f44a98715dc5c9381a744344607331b
Nilov Bogdan <bnilov106@gmil.com>
2024-07-06 14:22:34 +0300

    completed homework

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,21 +1,26 @@
 fun main() {
-    println(calculateTransfer("Visa", 500000, 100))
+    println(calculateTransfer("Mir", 0, 10000))
 }
 
 fun calculateTransfer(cardTipe: String, transferSumMonth: Int, nextTransfer: Int): String {
-    val maxLimitDay = 150000
+    val maxLimitDay = 150_000
     val maxLimitMonth = 600000
     var different = 0
     if (transferSumMonth + nextTransfer < maxLimitMonth && nextTransfer < maxLimitDay) {
         if (cardTipe == "Mastercard") {
-            if (transferSumMonth + nextTransfer > 75000 && nextTransfer > 75000) {
-                different = (((nextTransfer - 75000) / 100 * 0.6) + 20).toInt()
-            } else different = 0
+            when {
+                nextTransfer + transferSumMonth <= 75000 -> different = 0
+                transferSumMonth <= 75000 && nextTransfer + transferSumMonth > 75000 ->
+                    different = (((nextTransfer + transferSumMonth - 75000) / 100 * 0.6) + 20).toInt()
+
+                transferSumMonth > 75000 -> different = (nextTransfer / 100 * 0.6 + 20).toInt()
+            }
         } else if (cardTipe == "Visa") {
-            if (nextTransfer / 100 * 0.75 < 35) {
-                different = 35
-            } else different = (nextTransfer / 100 * 0.75).toInt()
+            when {
+                nextTransfer / 100 * 0.75 < 35 -> different = 35
+                nextTransfer / 100 * 0.75 >= 35 -> different = (nextTransfer / 100 * 0.75).toInt()
+            }
         }
     } else return " Перевод не может быть выполнен, превышен лимит!"
-    return "Перевод выполнен, комиссия составляет " + different + " руб."
+    return "Перевод на сумму " + nextTransfer + " руб. выполнен, комиссия составляет " + different + " руб."
 }
